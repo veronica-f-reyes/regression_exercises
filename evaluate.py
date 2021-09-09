040d8a279d21c2d4731934c6334aa718cb72def2
Veronica F Reyes <veronica.fierro.reyes1@gmail.com>
2021-09-09 16:59:44 -0500

    Working on Feature Engineering Exercises

--- evaluate.py
+++ evaluate.py
@@ -113,3 +113,59 @@ def better_than_baseline(y, yhat):
         return True
     else:
         return False
+
+
+#From exercise review:
+def plot_residuals(actual, predicted):
+    residuals = actual - predicted
+    plt.hlines(0, actual.min(), actual.max(), ls=':')
+    plt.scatter(actual, residuals)
+    plt.ylabel('residual ($y - \hat{y}$)')
+    plt.xlabel('actual value ($y$)')
+    plt.title('Actual vs Residual')
+    plt.show()     
+
+def residuals(actual, predicted):
+    return actual - predicted
+
+def sse(actual, predicted):
+    return (residuals(actual, predicted) **2).sum()
+
+def mse(actual, predicted):
+    n = actual.shape[0]
+    return sse(actual, predicted) / n
+
+def rmse(actual, predicted):
+    return math.sqrt(mse(actual, predicted))
+
+def ess(actual, predicted):
+    return ((predicted - actual.mean()) ** 2).sum()
+
+def tss(actual):
+    return ((actual - actual.mean()) ** 2).sum()
+
+def r2_score(actual, predicted):
+    return ess(actual, predicted) / tss(actual)       
+
+
+def regression_errors(actual, predicted):
+    return pd.Series({
+        'sse': sse(actual, predicted),
+        'ess': ess(actual, predicted),
+        'tss': tss(actual),
+        'mse': mse(actual, predicted),
+        'rmse': rmse(actual, predicted),
+    })
+
+def baseline_mean_errors(actual):
+    predicted = actual.mean()
+    return {
+        'sse': sse(actual, predicted),
+        'mse': mse(actual, predicted),
+        'rmse': rmse(actual, predicted),
+    }
+
+def better_than_baseline(actual, predicted):
+    rmse_baseline = rmse(actual, actual.mean())
+    rmse_model = rmse(actual, predicted)
+    return rmse_model < rmse_baseline    
